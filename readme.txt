533e9d826b263c10422aae9c0378b3c63d4a7b42
Tim Twigg <tim@twiggusa.com>
2023-01-19 16:33:08 -0800

    Dev 1/19
    Work on responsiveness
    Add fading content

--- readme.txt
+++ readme.txt
@@ -35,12 +35,13 @@ Talking to classmates about class material, assignment requirements, etc. is a g
 
 1/17: 4
 1/18 3
-1/19 1.5
+1/19 4.5
 
 4. What online resources did you consult when completing this assignment? (list specific URLs)
 
 https://www.w3schools.com/howto/howto_js_topnav_responsive.asp
 https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+https://stackoverflow.com/questions/26694385/fade-in-on-scroll-down-fade-out-on-scroll-up-based-on-element-position-in-win
 
 5. What classmates or other individuals did you consult as part of this assignment? What did you discuss?
 
--- script.js
+++ script.js
@@ -9,19 +9,46 @@ function changeNavBar() {
     }
 }
 
+function hideResponsiveNavBar() {
+    var x = document.getElementById("home");
+    x.className = "topnav";
+}
+
+
 /* Manage toTopButton */
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
+    /* toTopButton */
     let mybutton = document.getElementById("toTopButton");
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         mybutton.style.display = "block";
     } else {
         mybutton.style.display = "none";
     }
-  }
+}
 
 function toTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+
+/* Manage Fading */
+$(window).on("load",function() {
+    $(window).scroll(function() {
+        var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+        $(".fade").each(function() {
+            /* Check the location of each desired element */
+            var objectBottom = $(this).offset().top + ($(this).outerHeight());
+            var objectMid = $(this).offset().top + ($(this).outerHeight() / 2);
+        
+            /* Fade in the element */
+            if ((objectMid < windowBottom) || (objectBottom < windowBottom)) { //object comes into view (scrolling down)
+                if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+            } else { //object goes out of view (scrolling up)
+                if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+            }
+        });
+    }).scroll(); //invoke scroll-handler on page-load
+});
